87d347c8f1f25e76c4c26f5d79f93d237867ebea
tofaelKabir <tofaelkabir@gmail.com>
2018-12-18 20:36:27 -0500

    tuesday

--- src/unitTest/Calculator.java
+++ src/unitTest/Calculator.java
@@ -2,7 +2,12 @@ package unitTest;
 
 public class Calculator {
     public int addition(int a, int b){
-        int total = a+b;
-        return total;
+        int total1 = a+b;
+        return total1;
+    }
+    public int subtraction (int a, int b){
+        int total2 = a-b;
+        return total2;
+
     }
 }
--- src/unitTest/TestCalculator.java
+++ src/unitTest/TestCalculator.java
@@ -1,10 +1,15 @@
 package unitTest;
 
+import org.testng.Assert;
+
 public class TestCalculator {
     public static void main(String[] args) {
         Calculator cal = new Calculator();
-        int result1 = cal.addition(10, 15);
-        Asser
+        int result1 = cal.addition(15, 10);
+        Assert.assertEquals(25, result1);
+        int result2 = cal.subtraction(35,30);
+        Assert.assertEquals(5, result2);
+
     }
 }
-//not done yet
+//how to make a directory, didn't find it , so created lib in package
